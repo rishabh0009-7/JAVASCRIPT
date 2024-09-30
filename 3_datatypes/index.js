060f05edca4d61cbf2618b672fbcd513decd2f05
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-09-30 20:15:52 +0530

    Create index.js

---
+++ 3_datatypes/index.js
@@ -0,0 +1,28 @@
+//  datatypes
+"use strict"; // treat all js code as newer version of js
+
+//alert (3+3) - will not work here becoz ye browser mai use hota hai iska alag syntaz hai yaha //we are using nodejs not browser
+
+// ecmascript - js standards for writing code
+
+// in js no need to write dataype same like python
+
+let name = "rishabh";
+let age = 20;
+let islogin = false;
+
+//in js we have very less datatypes
+//primitive
+
+//numbers --> 2 to power 53
+//bigint
+//string --> ""
+//boolean -->true/false
+//null --> ek standalone value hai // representation hai empty value ka
+let state = null;
+//undefined --> matlab abhi value assign nhi hui hai
+//symbol -->used for uniqueness
+
+//type of --> this will tell the type of data
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
