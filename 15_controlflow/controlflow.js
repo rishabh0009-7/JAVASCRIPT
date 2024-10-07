061d5fc2e2660685bfe3e7bfb173f7f6390e7426
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-10-07 20:13:48 +0530

    Create controlflow.js

---
+++ 15_controlflow/controlflow.js
@@ -0,0 +1,9 @@
+//control flow in js 
+
+//abhi tak jo bhi kiya usme  pura file ka code run ho raha hai par hamesha aisa nhi hona chahiye hamesha code conditional basis pe run hona chahiye 
+
+//if  condn true tabhi vo execute hoga 
+if (true){
+
+
+}
