60f42dd36bccfd8c9b0593c4e928730b18d515a5
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-10-11 14:39:43 +0530

    Create loops2.js

---
+++ 16_loops/loops2.js
@@ -0,0 +1,32 @@
+// while and do while loop in js 
+  //while loop 
+// let i = 0;
+// while (i<=10){
+//     console.log(i);
+//     i++;
+    
+// }
+
+// let myArray = ["flash","batman","superman"]
+// let arr = 0 
+// while (arr<myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr++
+// }
+
+//do while 
+// let score = 0
+// do {
+//     console.log(`score is ${score }`);
+//     score ++
+// } while (score <=10);
+
+//do while ka fayda kaam pehle hota hai condn last mai check hoti hai 
+let score = 11
+do {
+    console.log(`score is ${score }`);
+    score ++
+} while (score <=10);
+
+//output score is 11 print hojayega 
+
