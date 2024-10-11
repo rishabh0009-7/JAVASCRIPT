08f7a890ade512c8a407128ba095d3e744b098e3
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-10-11 16:54:51 +0530

    Create dom.js

---
+++ 18_dom/dom.js
@@ -0,0 +1,16 @@
+//dom manipulation (chedchad)--> console mai jakar 
+// dom html 
+
+// window --> document --> html 
+//html - 2 parts --> head ,body
+//head - 2 part --> title , meta 
+//title - > text node 
+//meta -->attribute
+
+
+//body -->div --> attribute 
+//div - 2 child --> h1 ,p
+//h1--> text node , attribute 
+//p -->text node 
+
+//note -->html collections , node collection ,array ye sab alag alag cheez hai z
