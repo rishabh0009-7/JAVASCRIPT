b633a3ffe1e902e5eb78fae8bfbc20ea6458d202
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-10-14 18:49:03 +0530

    Create api.js

---
+++ 21_apirequestandv8engine/api.js
@@ -0,0 +1,8 @@
+ //some famous apis 
+ //apigithub
+ //randomuser.me
+
+ //api padhna hai kaafi difficult isliye we use json formatter
+
+ //before studying fetch we will study some old methods
+ //xmlhttprequest-->it was used heavily in ajax programming
